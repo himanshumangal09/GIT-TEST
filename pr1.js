0b398444bfe24e40286f6de7e9a5e7fd15e37b80
9313506071 <46906894+9313506071@users.noreply.github.com>
2019-07-16 15:56:55 +0530

    Update pr1.js

--- pr1.js
+++ pr1.js
@@ -5,8 +5,8 @@ function submitanswers() {
     var q1=document.forms["quizform"]["q1"].value ;
     var q2=document.forms["quizform"]["q2"].value ;
     var q3=document.forms["quizform"]["q3"].value ;
-    var q4=document.forms["quizform"]["q3"].value ;
-    var q5=document.forms["quizform"]["q4"].value ;
+    var q4=document.forms["quizform"]["q4"].value ;
+    var q5=document.forms["quizform"]["q5"].value ;
  for(i=1; i<=total ; i++)
  {
      if(eval('q'+i)== null||eval('q'+i)=='')
